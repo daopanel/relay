e14b6a30a46a05ccd7a425a51efc1500987bc870
roycepdev <roycepdev@gmail.com>
2022-07-26 10:37:21 -0700

    Adding more characters

--- src/hooks/useResponsiveUserId.ts
+++ src/hooks/useResponsiveUserId.ts
@@ -27,7 +27,7 @@ export const useResponsiveUserId = (
 };
 
 function truncated(str: string): string {
-  if (str.length < 9) {
+  if (str.length < 13) {
     return str;
   } else {
     return str.slice(0, 6) + '...' + str.slice(-4);
